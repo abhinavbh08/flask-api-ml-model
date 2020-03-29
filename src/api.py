f78038997a1811c1e6290c9d3b7107e61c526169
abhinavbh08 <abhinavbhatt.999@gmail.com>
2020-03-29 18:15:49 +0530

    Added the file for prediction and also fixed some small bugs

--- src/api.py
+++ src/api.py
@@ -8,12 +8,12 @@ loaded_model = tf.keras.models.load_model("models/my_mnist_model.h5")
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    if request.method=="POST":
 
+    if request.method=="POST":
         file = request.files["file"]
         img_bytes = file.read()
         class_id = get_predictions(loaded_model, img_bytes)
-        return jsonify({"class": "This item has been identified to belong to " + class_id})
+        return jsonify({"class": "This item has been identified to belong to " + str(class_id)})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 def transform_image(image):
+    image = np.array(image)
     image = image/255.0
     return np.expand_dims(image, axis=0)
